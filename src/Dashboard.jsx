92ac40d8122fe0d8c700daf3431878f9e3a5e1fd
BAISIG <baisig246@gmail.com>
2025-09-13 17:56:39 -0400

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createClient } from '@supabase/supabase-js';
+import isleImage from '../isle4.png';
 
 // Initialize Supabase client
 const supabase = createClient(
